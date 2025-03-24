b6f0a36861972ee6f3959a652c82a9a3d4459c9e
LCh-Salgado <lilianachicas180@gmail.com>
2025-03-24 14:46:51 -0400

    do the strings file

--- app/src/main/java/org/baltimorecityschools/hopeofeat/ListDonationMainActivity.java
+++ app/src/main/java/org/baltimorecityschools/hopeofeat/ListDonationMainActivity.java
@@ -1,6 +1,9 @@
 package org.baltimorecityschools.hopeofeat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ListDonationMainActivity extends AppCompatActivity {
 
+    Button seeLocationDBTN;
+    Button listPlacesBTN;
+
+    Intent nextScreenMap;
+    Intent nextScreenListPlaces;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_donation_main);
 
+        seeLocationDBTN = (Button)findViewById(R.id.SeeLoBTN);
+        listPlacesBTN = (Button)findViewById(R.id.listPlacesBTN);
+
+        seeLocationDBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextScreenMap = new Intent(ListDonationMainActivity.this, MapsActivity.class);
+                startActivity(nextScreenMap);
+            }
+        });
+        listPlacesBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextScreenListPlaces = new Intent(ListDonationMainActivity.this,
+                        ListDonationMainActivity.class);
+                startActivity(nextScreenListPlaces);
+            }
+        });
     }
 }
